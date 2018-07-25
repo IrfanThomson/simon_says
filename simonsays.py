406c8aa08b170ee32988a7208e78d3ce1f9f486f
IrfanThomson <irfanthomson@gmail.com>
2018-07-25 09:28:45 -0500

    final version of game

--- simonsays.py
+++ simonsays.py
@@ -2,6 +2,7 @@ import RPi.GPIO as GPIO
 import LEDRGB as LED
 import time
 import random
+from getpass import getpass
 #this script demonstrates looping through a list
 #breadboard setup
 GPIO.setmode(GPIO.BOARD)
@@ -23,11 +24,27 @@ R_pin = 11
 G_pin = 12
 B_pin = 13
 colorlist = []
+color_s = []
+
 LED.setup(R_pin, G_pin, B_pin)
+
+def validateGuess(color, guess):
+	if color == guess.upper():
+		print "Correct!"
+	else:
+		print "You lose!"
+		print "It was",color,"and you guessed",guess.upper()
+		print "You were on level",len(color_s)
+		LED.destroy()
+		Buzz.stop()
+		exit()
+		
+	
 def append_list():
 	while True:
 		n = random.randint(0,3)
 		colorlist.append(n)
+		color_s.append(colors[n])
 		for i in colorlist:
 			LED.setColor(colors[i])
 			Buzz.ChangeFrequency(frequencies[i])
@@ -36,7 +53,9 @@ def append_list():
 			LED.noColor()
 			Buzz.stop()
 			time.sleep(0.5)
-
+		guess = getpass("Guess the color sequence: ")
+		colorString = ''.join(color_s)
+		validateGuess(colorString, guess)
 if __name__ == '__main__':
 	try:
 		append_list()
